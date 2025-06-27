c66c9f7149dbc61f677ea5ed61f725f2df3ef704
EmmaPoppelierWSBD <e.poppelier@brabantsedelta.nl>
2025-06-27 14:12:52 +0200

    opmaak

--- scripts/afwateringseenheden/00-bewerk_ahn.py
+++ scripts/afwateringseenheden/00-bewerk_ahn.py
@@ -1,9 +1,11 @@
 # %%
+from pathlib import Path
+
 import rasterio
 from rasterio.enums import Resampling
-from afwateringseenheden.fnames import load_env_to_dict
 from rasterio.fill import fillnodata
-from pathlib import Path
+
+from wbd_tools.fnames import load_env_to_dict
 
 cell_size = 2
 
@@ -13,12 +15,8 @@ with rasterio.open(ahn_dir.joinpath("dtm_05m", "dtm_05m.vrt")) as src:
     scale_factor = cell_size / abs(src.res[0])
     new_height = int(src.height / scale_factor)
     new_width = int(src.width / scale_factor)
-    resampled_data = src.read(
-        out_shape=(src.count, new_height, new_width), resampling=Resampling.bilinear
-    )
-    resampled_transform = src.transform * src.transform.scale(
-        (src.width / new_width), (src.height / new_height)
-    )
+    resampled_data = src.read(out_shape=(src.count, new_height, new_width), resampling=Resampling.bilinear)
+    resampled_transform = src.transform * src.transform.scale((src.width / new_width), (src.height / new_height))
 
     profile = src.profile.copy()
     profile.update(
