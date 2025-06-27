eef74f3ecc98881277888724744f88504ea59827
EmmaPoppelierWSBD <e.poppelier@brabantsedelta.nl>
2025-06-27 16:26:36 +0200

    create_vrt_ahn5

--- scripts/afwateringseenheden/00-ahn_vrt.py
+++ scripts/afwateringseenheden/00-ahn_vrt.py
@@ -1,32 +1,11 @@
 # %%
 from pathlib import Path
 
-import geopandas as gpd
-import numpy as np
-import rasterio
-import requests
 from osgeo import gdal
-from rasterio.enums import Resampling
-from rasterio.io import MemoryFile
-from shapely.geometry import Polygon
 
 # %%%
-ROOT_URL = "https://service.pdok.nl/rws/ahn/atom/downloads"
-
-
-def get_tiles_gdf(poly_mask: Polygon | None = None, ahn_type: str = "dtm_05m"):
-    # get url
-    response = requests.get(f"{ROOT_URL}/{ahn_type}/kaartbladindex.json")
-    response.raise_for_status()
-
-    # convert to gdf
-    gdf = gpd.GeoDataFrame.from_features(response.json(), crs=28992)
-
-    # clip gdf
-    if poly_mask is not None:
-        gdf = gdf[gdf.intersects(poly_mask)]
-
-    return gdf
+fnames = get_fnames()
+ahn_dir = fnames["ahn_dir"]
 
 
 def create_vrt_file(download_dir: Path):
@@ -49,69 +28,6 @@ def create_vrt_file(download_dir: Path):
     ds.FlushCache()
 
 
-def get_ahn_rasters(
-    download_dir: Path,
-    poly_mask: Polygon | None = None,
-    ahn_type: str = "dtm_05m",
-    missings_only: bool = True,
-    create_vrt: bool = True,
-    save_tiles_index: bool = False,
-):
-    # get AHN tiles as gdf
-    tiles_gdf = get_tiles_gdf(poly_mask, ahn_type=ahn_type)
+create_vrt_file(ahn_dir)
 
-    # make download dir
-    download_dir = Path(download_dir)
-    download_dir = download_dir / ahn_type
-    download_dir.mkdir(exist_ok=True, parents=True)
-
-    # save index tiles
-    if save_tiles_index:
-        tiles_gdf.to_file(download_dir / f"{ahn_type}.gpkg")
-
-    # iteratively download AHN-tiles
-    for row in tiles_gdf.itertuples():
-        file_path = download_dir / f"{row.kaartbladNr}.tif"
-
-        if missings_only and (not file_path.exists()):
-            print(f"downloading {row.kaartbladNr}")
-
-            # get file
-            try:
-                response = requests.get(row.url)
-                response.raise_for_status()
-            except:
-                continue
-
-            # read tif in memory
-            with MemoryFile(response.content) as memfile:
-                with memfile.open() as src:
-                    # Read the data
-                    data = src.read(1)  # Read first band; use read() for all bands
-
-                    # make it integer
-
-                    data = np.where(data != src.nodata, (data * 100), -32768).astype(np.int16)
-
-                    # update profile
-                    profile = src.profile.copy()
-                    profile.update(
-                        dtype=np.int16,
-                        compress="deflate",
-                        predictor=2,
-                        tiled=True,
-                        driver="GTiff",
-                        nodata=-32768,
-                    )
-
-                    # write it to disc
-
-                    with rasterio.open(file_path, "w", **profile) as dst:
-                        cell_size = abs(dst.res[0])
-                        dst.scales = (0.01,)
-                        dst.write(data, 1)
-                        factors = [int(size / cell_size) for size in [5, 25] if size > cell_size]
-                        dst.build_overviews(factors, Resampling.average)
-                        dst.update_tags(ns="rio_overview", resampling="average")
-    if create_vrt:
-        create_vrt_file(download_dir)
+# %%
