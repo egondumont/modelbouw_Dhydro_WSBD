d431466aa185cb8f602f61051e09a4cb09e5abe2
egondumont <e.dumont@brabantsedelta.nl>
2025-07-08 19:47:41 +0200

    code toegevoegd die meetpunt op bovenstrooms hydroobject plaatst als gemaal net benedenstrooms van het eindpunt van een ander hydroobject ligt, i.p.v. het meetpunt te plaatsen op het hydroobject waarop het gemaal ligt.

--- wbd_tools/process_damo/process_management.py
+++ wbd_tools/process_damo/process_management.py
@@ -21,7 +21,8 @@ class ProcessManagement:
             geometry=[],
             crs="EPSG:28992",
         )
-
+        # Assumed distance (meters) between pump station en the observation station used for steering the pump
+        distance_pump_observation = 3.0
         network_buffer = network.buffer(self.checkbuffer[0], cap_style=2)
         pumpindex_hydrobjectindex = {}
 
@@ -36,9 +37,29 @@ class ProcessManagement:
                 if row_pumpstation["code"] == row_management["pompid"]:
                     # selecteren hydroobject waarop het gemaal ligt
                     closest_branch = network.iloc[pumpindex_hydrobjectindex[index_pumpstation]]
-                    # find location on closest branch that is 3 meters upstream of the pumpstation
-                    distance = closest_branch.geometry.project(row_pumpstation.geometry) - 3.0
-                    observation_location = Point(closest_branch.interpolate(distance).geometry.iloc[0])
+                    # find location of pumpstation on closest branch
+                    distance = closest_branch.geometry.project(row_pumpstation.geometry)
+                    # find location of observation on branch on specified distance usptream of pumpstation
+                    distance = (distance - distance_pump_observation).iloc[0]
+                    # if the beginning of the branch is within 3 meters:
+                    # assign a location on the preceding branch, or
+                    # if there is no preceding branch, assign the beginning of the current branch
+                    if distance < 0.0:
+                        # find preceding branch
+                        closest_branch_ = self.point_near_linestring_end(
+                            row_pumpstation.geometry, network.geometry, distance_pump_observation
+                        )
+                        # if preceding branch is found:
+                        if closest_branch_:
+                            closest_branch = closest_branch_
+                        # If not, then put observation on start of current hydroobject
+                        else:
+                            distance = 0.0
+
+                    # convert distance from beginning of branch to Shapely Point
+                    observation_location = closest_branch.interpolate(distance)
+                    if not isinstance(observation_location, Point):
+                        observation_location = observation_location.iloc[0]
                     row_observations = gpd.GeoDataFrame(
                         {"id": [row_pumpstation.code], "locationtype": ["1d"]},
                         geometry=[observation_location],
@@ -52,3 +73,25 @@ class ProcessManagement:
 
         observations.to_file(self.output_dir / "meetpunten.gpkg", driver="GPKG")
         management.to_file(self.output_dir / "sturing.gpkg", driver="GPKG")
+
+    def point_near_linestring_end(self, point, linestrings, distance_tolerance):
+        """
+        Checks if a Shapely Point is within a specified distance of a LineString's endpoint.
+
+        Args:
+            point: A shapely.geometry.Point object.
+            linestrings: A shapely.geoseries.LineString object.
+        `   distance_tolerance: The maximum distance to consider the point near the endpoint.
+
+        Returns:
+            If the point is within the distance tolerance of either endpoint the linestring with this end point is returned, None otherwise.
+        """
+        index_distance = {}
+        for index, linestring in linestrings.items():
+            end_point = Point(linestring.coords[-1])
+            if point.distance(end_point) <= distance_tolerance:
+                index_distance[index] = point.distance(end_point)
+        if not index_distance:
+            return None
+        else:
+            return linestrings[min(index_distance, key=index_distance.get)]
